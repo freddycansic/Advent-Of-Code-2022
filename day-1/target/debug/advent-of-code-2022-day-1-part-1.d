447afbf3ad0b79ad7744941f23922f2b2edf8c38
Freddy Cansick <freddycansick@gmail.com>
2022-12-03 18:37:19 +0000

    Added .gigignore

--- day-1/target/debug/advent-of-code-2022-day-1-part-1.d
+++
@@ -1,1 +0,0 @@
-/home/freddycansick/Documents/Advent-Of-Code-2022/day-1/target/debug/advent-of-code-2022-day-1-part-1: /home/freddycansick/Documents/Advent-Of-Code-2022/day-1/src/main.rs
--- day-1/target/debug/deps/advent_of_code_2022_day_1_part_1-439b024d8fedd138.d
+++
@@ -1,5 +0,0 @@
-/home/freddycansick/Documents/Advent-Of-Code-2022/day-1/target/debug/deps/advent_of_code_2022_day_1_part_1-439b024d8fedd138: src/main.rs
-
-/home/freddycansick/Documents/Advent-Of-Code-2022/day-1/target/debug/deps/advent_of_code_2022_day_1_part_1-439b024d8fedd138.d: src/main.rs
-
-src/main.rs:
