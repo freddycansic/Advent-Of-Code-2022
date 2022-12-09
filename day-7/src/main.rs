2afd77f1b67eec91d915022eb47ab77054cb8b8a
Freddy Cansick <freddycansick@gmail.com>
2022-12-09 09:27:09 +0000

    Day 7

--- day-7/src/main.rs
+++ day-7/src/main.rs
@@ -1,38 +1,97 @@
-use std::{fs, collections::HashMap, fmt::Display};
+use std::{fs, collections::HashMap};
 
-fn print_list<T>(iter : impl Iterator<Item=T>)
-where T : Display
-{
-    for item in iter
-    {
-        println!("{item}");
-    }
-}
-
-fn main() 
+fn main()
 {
     let input = fs::read_to_string("input.txt").expect("Could not find input.txt!");
 
     let lines : Vec<&str> = input.split("\n").collect();
     let lines : Vec<&str> = lines.into_iter().filter(|line| line.split(" ").collect::<Vec<&str>>()[1] != "ls").collect();
-    let lines : Vec<Vec<&str>> = lines.iter().map(|line| line.split(" ").collect::<Vec<&str>>()).collect();
 
-    let lines : Vec<Vec<String>> = lines.iter().map(|line| line.iter().map(|token| token.to_string()).collect()).collect();
-
-    let mut directory_sizes = HashMap::<String, u32>::new();
+    let mut directories = HashMap::<Vec<&str>, (Vec<Vec<&str>>, u32)>::new();
     
-    let mut current_directory : String = String::new(); 
+    let mut current_directory_path : Vec<&str> = Vec::new();
 
     for line in lines.iter()
     {
-        if line[0] == "cd".to_string()
+        let tokens : Vec<&str> = line.split(" ").collect();
+        let command : &str = tokens[0];
+        
+        match command
         {
-            current_directory += &("/".to_string() + &line[1].to_string());
-        }
-        else
-        {
-            let current_size = directory_sizes.get(&current_directory).unwrap();
-            directory_sizes.insert(current_directory, current_size + line[0].parse::<u32>().unwrap());
+            "$" => // cd
+            {
+                if tokens[2] == ".."
+                {
+                    current_directory_path.pop();
+                } 
+                else 
+                {
+                    current_directory_path.push(tokens[2]);
+                }
+            }
+
+            "dir" => 
+            {
+                let mut current_directory : (Vec<Vec<&str>>, u32) = (Vec::new(), 0);
+
+                if directories.contains_key(&current_directory_path)
+                {
+                    current_directory = directories.get(&current_directory_path).unwrap().clone();
+                }
+
+                let mut subdir_path = current_directory_path.clone();
+                subdir_path.push(tokens[1]);
+
+                current_directory.0.push(subdir_path.clone());
+
+                directories.insert(current_directory_path.clone(), current_directory);
+            }
+
+            _ => // file
+            {
+                let mut current_directory : (Vec<Vec<&str>>, u32) = (Vec::new(), 0);
+
+                if directories.contains_key(&current_directory_path)
+                {
+                    current_directory = directories.get(&current_directory_path).unwrap().clone();
+                }
+
+                directories.insert(current_directory_path.clone(), (current_directory.0,  current_directory.1 + tokens[0].parse::<u32>().unwrap()));
+            }
         }
     }
+
+    let directories_temp = directories.clone();
+    let mut directories_sorted : Vec<(&Vec<&str>, &(Vec<Vec<&str>>, u32))>= directories_temp.iter().map(|(key, val)| (key, val)).collect();
+
+    directories_sorted.sort_by(|dir1, dir2| dir1.0.len().cmp(&dir2.0.len()));
+    directories_sorted.reverse();
+
+    for directory in directories_sorted.iter()
+    {
+        if directory.1.0.len() <= 0 { continue; } // no subdirs
+     
+        let mut subdirectories_size = 0;
+
+        for subdirectory in directory.1.0.iter()
+        {
+            subdirectories_size += directories.get(&subdirectory.clone()).unwrap().1;
+        }
+
+        directories.insert(directory.0.to_vec(), (directory.1.0.clone(), directory.1.1 + subdirectories_size));
+    }
+
+    let part_1 : u32 = directories.iter().filter(|(_key, value)| value.1 <= 100_000 as u32).map(|(_key, value)| value.1).sum::<u32>();
+
+    println!("Part 1 : {part_1}");
+
+    let total_size_used = directories.get(&["/"].to_vec()).unwrap().1;
+
+    let unused_space = 70_000_000 - total_size_used;
+
+    let space_needed = 30_000_000 - unused_space;
+    
+    let p2 = directories.iter().filter(|(_key, value)| value.1 >= space_needed).map(|(_key, value)| value.1).min().unwrap();
+
+    println!("Part 2 : {p2}");
 }
